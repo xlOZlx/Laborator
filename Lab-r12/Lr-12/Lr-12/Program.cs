88abaff00c9c0e67267a8eaf5c50ca0976ea4ee1
xlOZlx <dima1562.17@gmail.com>
2018-12-18 22:17:58 +0300

    Add Lab-r12 & LAb-r13

---
+++ Lab-r12/Lr-12/Lr-12/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace Lr_12
+{
+    public static class Reflector
+    {
+        // метод, выводящий содержимое в класс
+        public static void OutInfoIntoFile(Type explore)
+        {
+            using (FileStream fstream = new FileStream(@"E:\\Laborator\note.txt", FileMode.Create))
+            {
+                foreach (MemberInfo mi in explore.GetMembers())
+                {
+                    byte[] array = Encoding.Default.GetBytes(mi.DeclaringType + " " + mi.MemberType + " " + mi.Name);
+                    Console.WriteLine(mi.DeclaringType + " " + mi.MemberType + " " + mi.Name);
+                    fstream.Write(array, 0, array.Length);
+                }
+                Console.WriteLine("\nИнформация записана в файл.");
+            }
+        }
+        // метод, извлекающий информацию о публичных методах
+        public static void ExtractPublicMethods(Type explore)
+        {
+            MethodInfo[] publicMethods = new MethodInfo[explore.GetMethods().Length];
+            int index = 0;
+            foreach (MethodInfo method in explore.GetMethods())
+            {
+                if (method.IsPublic)
+                {
+                    publicMethods[index] = method;
+                    index++;
+                }
+            }
+            Console.WriteLine("Информация о публичных методах:\n");
+            foreach (MethodInfo method in publicMethods)
+            {
+                Console.Write("public " + method.ReturnType.Name + " " + method.Name + " (");
+                ParameterInfo[] parameters = method.GetParameters();
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    Console.Write(parameters[i].ParameterType.Name + " " + parameters[i].Name);
+                    if (i + 1 < parameters.Length) Console.Write(", ");
+                }
+                Console.WriteLine(")");
+            }
+        }
+        // метод, получающий информацию о полях и свойствах класса
+        public static void ExtractFieldsAndProperties(Type explore)
+        {
+            Console.WriteLine("Информация о полях и свойствах:\n");
+            foreach (FieldInfo field in explore.GetFields())
+            {
+                Console.WriteLine(field);
+            }
+            foreach (PropertyInfo property in explore.GetProperties())
+            {
+                Console.WriteLine(property);
+            }
+        }
+        // метод, получающий все реализованные классом интерфейсы
+        public static void ExtractInterfaces(Type explore)
+        {
+            Console.WriteLine("Информация об интерфейсах:\n");
+            foreach (Type i in explore.GetInterfaces())
+            {
+                Console.WriteLine(i.Name);
+            }
+        }
+        // метод, выводит по имени класса имена методов, которые содержат заданный (пользователем) тип параметра
+
+        public static void ExtractSpecificMethods(Type explore)
+        {
+            MethodInfo[] rightMethods = new MethodInfo[explore.GetMethods().Length];
+            int index = 0;
+            Console.Write("Введите тип параметра: ");
+            string parameterType = Console.ReadLine();
+            foreach (MethodInfo method in explore.GetMethods())
+            {
+                bool flag = false;
+                foreach (ParameterInfo parameter in method.GetParameters())
+                {
+                    if (parameter.ParameterType.Name == parameterType)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    rightMethods[index] = method;
+                    index++;
+                }
+            }
+            Console.WriteLine("Информация о конкретных методах:\n");
+            foreach (MethodInfo method in rightMethods)
+            {
+                if (method != null)
+                {
+                    Console.WriteLine(method.Name);
+                }
+            }
+        }
+        // метод, вызывает некоторый метод класса, при этом значения для его параметров необходимо прочитать из текстового файла
+        public static void MethodСall(string typeName, string methodName)
+        {
+            using (FileStream fstream = File.OpenRead(@"E:\\Laborator\par.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string parameter = Encoding.Default.GetString(array);
+                Type explore = Type.GetType($"Lr_12.{typeName}");
+                object obj = Activator.CreateInstance(explore);
+                MethodInfo method = explore.GetMethod(methodName);
+                Console.WriteLine("MethodCall:");
+                method.Invoke(obj, new object[] { parameter });
+            }
+        }
+
+
+    }
+
+    public partial class Customer : ICustomer
+    {
+        public string Name { get; set; }
+        public int age;
+        public int balanse;
+
+        public void Write()
+        {
+            Console.WriteLine("Я покупатель.");
+        }
+        public void Scream(string toScream)
+        {
+            Console.WriteLine(toScream + "!");
+        }
+
+        public double Credit(ref double balanc_in, out double balanc_out)
+        {
+            double i = balanc_in;
+            double plus;
+            Console.WriteLine("\nВведите количество зачисленных средств:");
+            plus = Convert.ToDouble(Console.ReadLine());
+            balanc_out = balanc_in + plus;
+
+            return i;
+        }
+    }
+
+    interface ICustomer
+    {
+        string Name { get; set; }
+        void Write();
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type explore = typeof(Customer);
+            Reflector.OutInfoIntoFile(explore);
+            Console.ReadLine();
+            Console.Clear();
+            Reflector.ExtractPublicMethods(explore);
+            Console.ReadLine();
+            Console.Clear();
+            Reflector.ExtractFieldsAndProperties(explore);
+            Console.ReadLine();
+            Console.Clear();
+            Reflector.ExtractInterfaces(explore);
+            Console.ReadLine();
+            Console.Clear();
+            Console.Write("Введите имя типа: ");
+            explore = Type.GetType($"Lr_12.{Console.ReadLine()}");
+            Reflector.ExtractSpecificMethods(explore);
+            Console.ReadLine();
+            Console.Clear();
+            Reflector.MethodСall("Customer", "Scream");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Пример с классом Console");
+            Reflector.ExtractPublicMethods(typeof(Console));
+            Console.ReadLine();
+            Console.Clear();
+            Reflector.ExtractFieldsAndProperties(typeof(Console));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab-r13/Lr-13/Lr-13/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace Lr_13
+{
+    internal class PDALog
+    {
+        ////    1111
+        public void MethodLog()
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(@"E:\\Laborator\PDAfileInfo.txt");
+                fileInfo.Refresh();
+
+                //var nameOfFile = fileInfo.Name;
+                //var fullNameOfFile = fileInfo.FullName;
+                //var timeOfCreate = fileInfo.CreationTime;
+                //var timeOfAccess = fileInfo.LastAccessTime;
+                //var existOfFile = fileInfo.Exists;
+
+                using (StreamWriter streamWriter = new StreamWriter(@"E:\\Laborator\PDAlogfile.txt", false, Encoding.Default))
+                {
+                    streamWriter.WriteLine($"имя файла: {/*nameOfFile*/fileInfo.Name}");
+                    streamWriter.WriteLine($"Полный путь к файлу: {/*fullNameOfFile*/fileInfo.FullName}");
+                    streamWriter.WriteLine($"Дата и время создания файла пользователем: {/*timeOfCreate*/fileInfo.CreationTime}");
+                    streamWriter.WriteLine($"Дата и время последнего изменения файла пользователем: {/*timeOfAccess*/fileInfo.LastAccessTime}");
+                    streamWriter.WriteLine($"Существование файла: {/*existOfFile*/fileInfo.Exists}");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        
+    }
+    ////    2222
+    internal class PDADiskInfo
+    {
+        public void MethodDiskInfo()
+        {
+            DriveInfo[] drifeInfo = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drife in drifeInfo)
+            {
+                try
+                {
+                    Console.WriteLine($"Свободное место на диске:\n{drife.Name} {drife.AvailableFreeSpace}");
+                    Console.WriteLine($"Общий объем памяти: {drife.TotalFreeSpace}");
+                    Console.WriteLine($"Метка тома: {drife.VolumeLabel}");
+                    Console.WriteLine($"Общий объем диска в байтах: {drife.TotalSize}");
+                    Console.WriteLine($"Файловая система: {drife.DriveFormat}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+    }
+    ////    3333
+    internal class PDAFileInfo
+    {
+        public void MethodFileInfo()
+        {
+            FileInfo fileInfo = new FileInfo(@"E:\\Laborator\PDAlogfile.txt");
+            Console.WriteLine($"\n\nИмя файла: {fileInfo.Name}");
+            Console.WriteLine($"Полный путь к файлу: {fileInfo.DirectoryName}");
+            Console.WriteLine($"Размер файла: {fileInfo.Length}");
+            Console.WriteLine($"Существование файла: {fileInfo.Extension}");
+            Console.WriteLine($"Время создания файла: {fileInfo.CreationTime}");
+        }
+    }
+    ////    4444
+    internal class PDADirInfo
+    {
+        public void MethodDirInfo()
+        {
+            Console.WriteLine("Вывод списка под каталогов E:\\2 курс");
+            string[] subDir = Directory.GetDirectories("E:\\2 курс");
+            foreach(string s in subDir)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Файлы E:\\2 курс:");
+            string[] files = Directory.GetFiles("E:\\2 курс");
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Кол-во подкаталогов E:\\2 курс:" + subDir.Length);
+
+            Console.WriteLine("Время создания каталога:");
+            var createDataTimeDir = Directory.GetCreationTime("E:\\2 курс");
+            Console.WriteLine();
+
+            var parent = Directory.GetParent("E:\\2 курс");
+            Console.WriteLine("Родительский каталог Каталога E:\\2 курс: " + parent);
+        }
+    }
+    ////    5555
+    internal class PDAFileManager
+    {
+        public void MethodFileManager()
+        {
+            Console.WriteLine("\n");
+            string[] listFiles = Directory.GetFiles("C:\\");
+            string[] listDirectories = Directory.GetDirectories("C:\\");
+
+            Console.WriteLine("\nСписок файлов каталога C");
+            foreach (string i in listFiles)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nСписок папок каталога C:");
+            foreach (string j in listDirectories)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.WriteLine("\nКаталог создан.");
+            Directory.CreateDirectory("E:\\Laborator\\PDAInspect");
+
+            Console.WriteLine("Создание файла и запись в него.");
+
+            using (StreamWriter streamWriter = new StreamWriter("E:\\Laborator\\PDAInspect\\PDAdirinfo.txt", true, Encoding.Default))
+            {
+                streamWriter.Write("Hello ''beautiful'' world!");
+                streamWriter.Close();
+            }
+
+            Console.WriteLine("\nКопия файла создана.");
+            File.Copy("E:\\Laborator\\PDAInspect\\PDAdirinfo.txt", "E:\\Laborator\\PDAInspect\\CopyPDAdirinfo.txt", false);
+
+            Console.WriteLine("Исходный файл удален.");
+            File.Delete("E:\\Laborator\\PDAInspect\\PDAdirinfo.txt");
+
+            Directory.CreateDirectory("E:\\Laborator\\PDAFiles");
+
+            File.Copy("E:\\Laborator\\PDAInspect\\CopyPDAdirinfo.txt", "E:\\Laborator\\PDAFiles\\NewCopyPDAdirinfo.txt", false);
+        }
+    }
+    ////    6666
+    internal class ViewPDAFiles
+    {
+        public void ViewPDALogFileMethod()
+        {
+            using (StreamReader streamReader = new StreamReader(@"E:\\Laborator\PDAlogfile.txt", Encoding.Default))
+            {
+                Console.WriteLine();
+                string stroka = streamReader.ReadToEnd();
+                Console.WriteLine(stroka);
+            }
+        }
+    }
+
+
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1111
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 1");
+            Console.ResetColor();
+
+            PDALog pdaLog = new PDALog();
+            pdaLog.MethodLog();
+
+            // 2222
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 2");
+            Console.ResetColor();
+
+            PDADiskInfo pdaDiskInfo = new PDADiskInfo();
+            pdaDiskInfo.MethodDiskInfo();
+
+            // 3333
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 3");
+            Console.ResetColor();
+
+            PDAFileInfo pdaFileInfo = new PDAFileInfo();
+            pdaFileInfo.MethodFileInfo();
+
+            // 4444
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 4");
+            Console.ResetColor();
+
+            PDADirInfo pdaDirInfo = new PDADirInfo();
+            pdaDirInfo.MethodDirInfo();
+
+            // 5555
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 5");
+            Console.ResetColor();
+
+            //PDAFileManager pdaFileManager = new PDAFileManager();
+            //pdaFileManager.MethodFileManager();
+
+            // 6666
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ЗАДАНИЕ 6");
+            Console.ResetColor();
+
+            ViewPDAFiles viewPDAFiles = new ViewPDAFiles();
+            viewPDAFiles.ViewPDALogFileMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
